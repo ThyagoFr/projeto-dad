981b127df603f1193e24e0ec9b108c9629d2b6de
marm10 <marcilio.ec.ufc@gmail.com>
2020-10-22 12:12:25 -0300

    correcao

--- frontend/server.js
+++ frontend/server.js
@@ -10,5 +10,13 @@ app.use(compression());
 
 // Serve the static files from the build folder
 app.use(express.static( __dirname + "/build"));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  });
+}
 // Listen to port 3000
 app.listen(3000);
